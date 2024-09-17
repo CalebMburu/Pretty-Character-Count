7908726af24661e6e9fb9f20359f0c55aa7a23c8
Caleb Mburu <127048369+CalebMburu@users.noreply.github.com>
2024-09-17 00:05:26 -0700

    Add files via upload

---
+++ prettyCharacterCount.py
@@ -0,0 +1,9 @@
+import pprint # Cleaner display of items in a dictionary that print()
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)
